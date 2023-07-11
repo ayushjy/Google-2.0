d5c9b5517dd6faa221e92b46e7fb90f2752b0dc7
Ayush jyoti <ayushjyoti0321@gmail.com>
2023-07-11 09:13:53 +0530

    changes all

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eb49759d5e94e467"; }
+/******/ 		__webpack_require__.h = function() { return "101fb9daadb82fb3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,36 @@
+import { useRouter } from 'next/router';
 import Head from "next/head"
 import Header from "../components/Header"
-const search = () => {
+import {API_KEY,CONTEXT_KEY} from "../keys"
+import Response from "../Response";
+import SearchResults from '@/components/SearchResults';
+
+const Search = ({results}) => {
+const router = useRouter();
   return (
     <div>
       <Head>
-        <title>search Results</title>
+        <title>{router.query.term} - Google Serach</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-    </div>
+      <SearchResults  results={results}/>
+    </div>  
   )
 }
 
-export default search
+export default Search
+
+export async function getServerSideProps(context){
+
+  const useDummyData= false;
+  const startIndex = context.query.start|| "0";
+   const data = useDummyData?Response: await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)
+   .then( (response) => response.json());
+   
+   return{
+    props:{
+      results:data,
+    }
+   }
+}
